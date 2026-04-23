2b53b23197ffbd3f8ed850c7b5f120a4b36db69d
FabioTelles11 <fabiounijorge0@gmail.com>
2026-04-22 23:47:51 -0300

    Clean up comments in server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = 4000;
 
-// Serve static files from 'frontend' directory
+
 app.use('/frontend', express.static(path.join(__dirname, 'frontend')));
 
-// Redirect root to index
+
 app.get('/', (req, res) => {
     res.redirect('/frontend/index.html');
 });
 
-// Catch-all handler for 404
+
 app.use((req, res) => {
     res.status(404).send('Página não encontrada. <a href="/frontend/index.html">Voltar ao início</a>');
 });
